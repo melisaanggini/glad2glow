ec59a8fa25f07af641486f314cfa70d9e6e282dc
melisaanggini <melisaanggini92@gmail.com>
2026-04-29 13:59:35 +0800

    update arsitektur kode

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-define('BASEURL', 'http://localhost/glad2glow/public');
-define('ROOT', dirname(__DIR__));
+define('BASEURL', 'http://localhost/glad2glow');
 
-// Simple Router
+define('ROOT', __DIR__);
+
 $request = $_SERVER['REQUEST_URI'];
-// strip query string
+
 $path = strtok($request, '?');
-// Remove base path if running in subdirectory
-$basePath = '/glad2glow/public';
+
+$basePath = '/glad2glow';
 $path = str_replace($basePath, '', $path);
+
 $path = rtrim($path, '/');
 if ($path === '') $path = '/';
 
@@ -33,7 +34,7 @@ switch ($path) {
     case '/product':
         require_once ROOT . '/app/controllers/ProductController.php';
         $ctrl = new ProductController();
-        // check if detail or listing
+
         if (isset($_GET['id'])) {
             $ctrl->detail($_GET['id']);
         } else {
@@ -43,6 +44,7 @@ switch ($path) {
 
     default:
         http_response_code(404);
-        echo "<h1>404 - Halaman tidak ditemukan</h1><a href='" . BASEURL . "'>Kembali ke Beranda</a>";
+        echo "<h1>404 - Halaman tidak ditemukan</h1>
+              <a href='" . BASEURL . "'>Kembali ke Beranda</a>";
         break;
 }
